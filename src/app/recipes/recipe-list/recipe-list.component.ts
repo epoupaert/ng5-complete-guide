a3b4eeaaee3660a304b4c6f008c73dd9836b0737
Eric Poupaert <epoupaert@gmail.com>
2018-03-22 10:47:36 +0100

    Added a Recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { RecipeService } from './../recipe.service';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from './../recipe.model';
@@ -9,22 +10,13 @@ import { Recipe } from './../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[];
   @Output() recipeSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    { name: 'A test recipe',
-      description: 'This is just a test recipe!',
-      imagePath: 'https://farm4.staticflickr.com/3112/3217904076_30162f05df_b.jpg'
-    },
-    { name: 'Another test recipe',
-      description: 'This is just a test recipe!',
-      imagePath: 'https://farm4.staticflickr.com/3112/3217904076_30162f05df_b.jpg'
-    }
-  ];
-
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe) {
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+import { Recipe } from './recipe.model';
+
+@Injectable()
+export class RecipeService {
+
+  private recipes: Recipe[] = [
+    { name: 'A test recipe',
+      description: 'This is just a test recipe!',
+      imagePath: 'https://farm4.staticflickr.com/3112/3217904076_30162f05df_b.jpg'
+    },
+    { name: 'Another test recipe',
+      description: 'This is just a test recipe!',
+      imagePath: 'https://farm4.staticflickr.com/3112/3217904076_30162f05df_b.jpg'
+    }
+  ];
+
+  constructor() { }
+
+  getRecipes() { return this.recipes.slice(); }
+
+}
