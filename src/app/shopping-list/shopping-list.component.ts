4d5dd2e337d729ac6d7219445818204f2671058b
Eric Poupaert <epoupaert@gmail.com>
2018-04-12 15:04:13 +0200

    Moved to Subject instead of EventEmitter

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,7 +1,8 @@
-import { ShoppingListService } from './shopping-list.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -9,15 +10,22 @@ import { Ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css'],
   providers: []
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+
+  private ingredientsChangedSubscription: Subscription;
 
   constructor(private shoppingListService: ShoppingListService) {}
 
   ngOnInit() {
     this.ingredients = this.shoppingListService.getIngredients();
-    this.shoppingListService.ingredientsChanged.subscribe(
-      (ingredients) => { this.ingredients = ingredients; }
-    );
+    this.ingredientsChangedSubscription = this.shoppingListService.ingredientsChanged
+      .subscribe(
+        (ingredients) => { this.ingredients = ingredients; }
+      );
+  }
+
+  ngOnDestroy() {
+    this.ingredientsChangedSubscription.unsubscribe();
   }
 }
