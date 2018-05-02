c6aca249efb2ea35c70366b5cc84d4c4151fb5fd
Eric Poupaert <epoupaert@gmail.com>
2018-05-02 14:48:57 +0200

    Support for deleting a Recipe

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { Recipe } from './../recipe.model';
 import { RecipeService } from './../recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -12,21 +12,29 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeDetailComponent implements OnInit {
 
   recipe: Recipe;
+  recipeId: number;
 
   constructor(
     private recipeService: RecipeService,
-    private route: ActivatedRoute) {}
+    private route: ActivatedRoute,
+    private router: Router) {}
 
   ngOnInit() {
     this.route.params.subscribe(
       (params: Params) => {
         const id = +params['id'];
         this.recipe = this.recipeService.getRecipe(id);
+        this.recipeId = id;
       }
     );
   }
 
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onDeleteRecipe() {
+    this.recipeService.deleteRecipe(this.recipeId);
+    this.router.navigate(['..'], {relativeTo: this.route});
   }
 }
