4767f415fbac411afe84d0bcd54696f2dfef9b47
Eric Poupaert <epoupaert@gmail.com>
2018-05-02 14:48:28 +0200

    Recipe: navigating away on Cancel or after saving

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
 import { Recipe } from './../recipe.model';
@@ -18,6 +18,7 @@ export class RecipeEditComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private recipeService: RecipeService) { }
 
   ngOnInit() {
@@ -85,6 +86,12 @@ export class RecipeEditComponent implements OnInit {
     } else {
       this.recipeService.addRecipe(newRecipe);
     }
+
+    this.router.navigate(['..'], {relativeTo: this.route});
+  }
+
+  onCancel() {
+    this.router.navigate(['..'], {relativeTo: this.route});
   }
 
   onAddIngredient() {
